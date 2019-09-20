42a0ea13de0751c47492f89332200466eeb885a6
Nikos Kostagiolas <nikos.kostagiolas@gmail.com>
2019-09-20 12:41:58 +0200

    Algorithm is up and running, just a little bit of debugging remains.

--- DDPG/agent.py
+++ DDPG/agent.py
@@ -2,6 +2,8 @@ import torch
 import torch.autograd
 import torch.optim as optim
 import torch.nn as nn
+import torch as T
+import gym
 from utils import *
 from model import *
 
@@ -9,14 +11,14 @@ from model import *
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class DDPGagent(object):
-	def __init__(self, env, gamma = 0.99, tau = 0.001, memory_capacity = 1e6,
+	def __init__(self, env, input_dims, nb_actions, gamma = 0.99, tau = 0.001, memory_capacity = 1e6,
 				actor_lr = 0.0001, critic_lr = 0.001, batch_size = 64):
 
 		self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 		self.env = env
 		# get number of possible states and actions
-		self.nb_states = env.obseration_space.shape[0]
+		self.nb_states = env.observation_space.shape[0]
 		self.nb_actions = env.action_space.shape[0]
 
 		# hyperparameters
@@ -28,23 +30,23 @@ class DDPGagent(object):
 		self.critic_lr = critic_lr
 
 		# initialize actor and critic networks, as well as their optimizers
-		self.actor = Actor(nb_states, nb_actions).to(device)
-		self.actor_target = Actor(nb_states, nb_actions).to(device)
+		self.actor = Actor(self.nb_states, self.nb_actions, self.actor_lr).to(device)
+		self.actor_target = Actor(self.nb_states, self.nb_actions, self.actor_lr).to(device)
 		# load the actor's parameter dictionary to critic_target
 		self.actor_target.load_state_dict(self.actor.state_dict())
-		self.actor_optimizer = optim.Adam(self.actor.parameters(), lr = actor_lr)
+		self.actor_optimizer = optim.Adam(self.actor.parameters(), lr = self.actor_lr)
 
-		self.critic = Critic(nb_states, nb_actions).to(device)
-		self.critic_target = Critic(nb_states, nb_actions).to(device)
+		self.critic = Critic(self.nb_states, self.nb_actions, self.critic_lr).to(device)
+		self.critic_target = Critic(self.nb_states, self.nb_actions, self.critic_lr).to(device)
 		# load the critic's parameter dictionary to critic_target
 		self.critic_target.load_state_dict(self.critic.state_dict())
-		self.cricit_optimizer = optim.Adam(self.critic.parameters(), lr = critic_lr)
+		self.cricit_optimizer = optim.Adam(self.critic.parameters(), lr = self.critic_lr)
 
 		# initialize experience replay memory
-		self.memory = ReplayMemory(self.memory_capacity)
+		self.memory = ReplayMemory(self.memory_capacity, input_dims, nb_actions)
 
 		# initialize random noisifying process
-		self.exploration_noise = OU_Noise(mu=np.zeros(self.nb_actions))
+		self.exploration_noise = OU_Noise(self.env.action_space, mu=np.zeros(self.nb_actions))
 
 	def select_action(self, observation):
 		# get an action from the actor and then apply noise
@@ -60,31 +62,33 @@ class DDPGagent(object):
 	def remember(self, s_t, a_t, s_t_, r_t, done):
 		self.memory.push_transition(s_t, a_t, s_t_, r_t, done)
 
-	def learn():
+	def learn(self):
 		if len(self.memory) < self.batch_size:
 			return
 	
 		# sample a batch of transitions from experience memory
 		s_t, a_t, s_t_, r_t, done = self.memory.sample(self.batch_size)
 
-		s_t = T.tensor(s_t, float).to(self.critic.device)
-		a_t = T.tensor(a_t, float).to(self.critic.device)
-		s_t_ = T.tensor(s_t_, float).to(self.critic.device)
-		r_t = T.tensor(r_t, float).to(self.critic.device)
+		s_t = T.tensor(s_t, dtype=T.float).to(self.critic.device)
+		a_t = T.tensor(a_t, dtype=T.float).to(self.critic.device)
+		s_t_ = T.tensor(s_t_, dtype=T.float).to(self.critic.device)
+		r_t = T.tensor(r_t, dtype=T.float).to(self.critic.device)
 		done = T.tensor(done).to(self.critic.device)
 
 		self.actor_target.eval()
 		self.critic_target.eval()
 		self.critic.eval()
 
-		# select an action according to current policy
-		# using the actor network, then compute its q value
-		# using the critic network
+		# depending on the new state, select a new action 
+		# according to current policy using the actor network
+		# then compute its q value using the critic network
 		a_t_ = self.actor_target.forward(s_t_)
 		q_value_ = self.critic_target.forward(s_t_, a_t_)
+
+		# also compute the q value for the current state
 		q_value = self.critic.forward(s_t, a_t)
 
-		# Set y_i = r_i + γ * q_value_. The new q value is computed
+		# Set y_i = r_i + γ * q_value_. The new q value was computed
 		# by passing the new actions chosen by the actor network
 		# through the critic network
 		y = []
@@ -104,16 +108,26 @@ class DDPGagent(object):
 
 		# Update actor by using the sampled policy gradient
 		self.actor.optimizer.zero_grad()
+		# First, compute the action for the current state
+		# according to the policy modeled by the actor network
 		mu = self.actor.forward(s_t)
 		self.actor.train()
+		# Compute the gradient of the actor loss by passing the
+		# states and policy-picked corresponding actions to the 
+		# critic network and averaging across this batch
 		actor_loss = -self.critic.forward(s_t, mu)
 		actor_loss = T.mean(actor_loss)
 		actor_loss.backward()
 		self.actor.optimizer.step()
 
-		self.update_network_parameters
+		self.update_network_parameters()
 
-	def update():
+	def update_network_parameters(self):
+
+		soft_update(self.actor_target, self.actor, self.tau)
+		soft_update(self.critic_target, self.critic, self.tau)
+		
+		"""
 		actor_params = self.actor.named_parameters()
 		critic_params = self.critic.named_parameters()
 		actor_target_params = self.actor_target.named_parameters()
@@ -124,6 +138,9 @@ class DDPGagent(object):
 		actor_target_state_dict = dict(actor_target_params)
 		critic_target_state_dict = dict(critic_target_params)
 
+		#print(actor_state_dict.size())
+		#print(actor_target_state_dict.size())
+		print(actor_target_state_dict)
 		for name in actor_target_state_dict:
 			actor_target_state_dict = self.tau * actor_state_dict[name] + (1 - self.tau) * actor_target_state_dict[name]
 
@@ -133,7 +150,7 @@ class DDPGagent(object):
 			critic_target_state_dict = self.tau * critic_state_dict[name] + (1 - self.tau) * critic_target_state_dict[name]
 
 		self.target_critic.load_state_dict(critic_state_dict)
-
+		"""
 
 
 
--- DDPG/model.py
+++ DDPG/model.py
@@ -2,11 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F 
 import torch.autograd
+import torch.optim as optim
 from torch.autograd import Variable
+import numpy as np
+import torch as T
+
+def fanin_init(size, fanin=None):
+    fanin = fanin or size[0]
+    v = 1. / np.sqrt(fanin)
+    return torch.Tensor(size).uniform_(-v, v)
 
 class Actor(nn.Module):
 	# The actor is the deterministic policy network
-	def __init__(self, nb_states, nb_actions, hidden_size1 = 400, 
+	def __init__(self, nb_states, nb_actions, lr, hidden_size1 = 400, 
 		hidden_size2 = 300):
 		super(Actor, self).__init__()
 
@@ -14,34 +22,42 @@ class Actor(nn.Module):
 		self.nb_states = nb_states
 		self.nb_actions = nb_actions
 
-		self.bn0 = nn.BatchNorm(hidden_size1)
+		#self.bn0 = nn.BatchNorm1d(nb_states)
 		self.fc1 = nn.Linear(nb_states, hidden_size1)
-		self.bn1 = nn.BatchNorm(hidden_size1)
+		#self.bn1 = nn.BatchNorm1d(hidden_size1)
 		self.relu1 = nn.ReLU()
 		self.fc2 = nn.Linear(hidden_size1, hidden_size2)
-		self.bn2 = nn.BatchNorm(hidden_size1)
+		#self.bn2 = nn.BatchNorm1d(hidden_size1)
 		self.relu2 = nn.ReLU()
 		self.out_fc3 = nn.Linear(hidden_size2, nb_actions)
 		self.output_tanh = nn.Tanh()
+		self.lr = lr
 		self.init_weights()
 
-	def init_weights(self):
-		self.fc1.weight.data = fanin_init(self.linear1.weight.data.size())
-        self.fc2.weight.data = fanin_init(self.linear2.weight.data.size())
-        # The paper states: the final weights and biases of the actor
-        # network were initialized from a uniform distribution [-3 x 1e-3, 3 x 1e-3]
-        self.out_fc3.weight.data.uniform_(-self.init_w, self.init_w)
+		self.optimizer = optim.Adam(self.parameters(), lr = self.lr)
 
-	def forward(self, states, actions):
+		self.device = T.device('cuda:0' if T.cuda.is_available() else 'cuda:1')
+
+		self.to(self.device)
+
+	def init_weights(self):
+		self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+		self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+		# The paper states: the final weights and biases of the actor
+		# network were initialized from a uniform distribution [-3 x 1e-3, 3 x 1e-3]
+		self.out_fc3.weight.data.uniform_(-self.init_w, self.init_w)
+
+	def forward(self, states):
 		# The paper states that for the deterministic policy network
 		# batch normalization was used on the state input and all the
 		# layers of the network
-		x = self.bn0(states)
-		x = self.fc1(x)
-		x = self.bn1(x)
+
+		#x = self.bn0(states)
+		x = self.fc1(states)
+		#x = self.bn1(x)
 		x = self.relu1(x)
 		x = self.fc2(x)
-		x = self.bn2(x)
+		#x = self.bn2(x)
 		x = self.relu2(x)
 		x = self.out_fc3(x)
 		# The paper states that the final output layer of the actor
@@ -52,8 +68,8 @@ class Actor(nn.Module):
 
 class Critic(nn.Module):
 	# The critic is the Q network
-	def __init__(self, hidden_size1 = 400, hidden_size2 = 300,
-		 nb_states, nb_actions, action_dim):
+	def __init__(self, nb_states, nb_actions, lr,
+		hidden_size1 = 400, hidden_size2 = 300):
 		super(Critic, self).__init__()
 
 		self.nb_states = nb_states
@@ -61,29 +77,38 @@ class Critic(nn.Module):
 		self.hidden_size1 = hidden_size1
 		self.hidden_size2 = hidden_size2
 		self.init_w = 3e-4
+		self.lr = lr
 
-		self.fc1 = nn.Linear(input_dim, hidden_size1)
-		self.bn1 = nn.BatchNorm(hidden_size1)
+		self.fc1 = nn.Linear(nb_states, hidden_size1)
+		#self.bn1 = nn.BatchNorm1d(hidden_size1)
 		self.relu1 = nn.ReLU()
 		self.fc2 = nn.Linear(hidden_size1 + nb_actions, hidden_size2)
-		self.bn2 = nn.BatchNorm(hidden_size2)
-		self.out_fc3 = nn.Linear(hidden_size2, output_size)
+		#self.bn2 = nn.BatchNorm1d(hidden_size2)
+		self.relu2 = nn.ReLU()
+		self.out_fc3 = nn.Linear(hidden_size2, 1)
 		self.init_weights()
 
+		self.optimizer = optim.Adam(self.parameters(), lr = self.lr)
+
+		self.device = T.device('cuda:0' if T.cuda.is_available() else 'cuda:1')
+
+		self.to(self.device)
+
 	def init_weights(self):
-        self.linear1.weight.data = fanin_init(self.linear1.weight.data.size())
-        self.linear2.weight.data = fanin_init(self.linear2.weight.data.size())
-        self.output.weight.data.uniform_(-self.init_w, self.init_w)
+		self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+		self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+		self.out_fc3.weight.data.uniform_(-self.init_w, self.init_w)
 
 	def forward(self, states, actions):
-		x = self.linear1(states)
-		x = self.bn1(x)
+
+		x = self.fc1(states)
+		#x = self.bn1(x)
 		x = self.relu1(x)
 		# The paper states that the actions were not included
 		# until the 2nd hidden layer of the Q network.
 		x = torch.cat((x, actions), dim = 1)
-		x = self.linear2(x)
-		x = self.bn2(x)
+		x = self.fc2(x)
+		#x = self.bn2(x)
 		x = self.relu2(x)
 		output = self.out_fc3(x)
 
